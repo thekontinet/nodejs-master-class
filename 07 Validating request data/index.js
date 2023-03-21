e6e6bb0a07f294c5d1032787c7792bd54febef17
rolomtech <support@rolomtech.com>
2023-03-21 13:49:24 +0100

    Validate POST request data

---
+++ 07 Validating request data/index.js
@@ -0,0 +1,115 @@
+const http = require('http')
+const url = require('url')
+const StringDecoder = require('string_decoder').StringDecoder
+
+// Creating a simple server that respond with hello world
+const server = http.createServer(function(req, res){
+    const parsedUrl = url.parse(req.url, true)
+
+    // Get the request path sent by the client
+    const requestPath = parsedUrl.pathname.replace(/^\/+|\/+$/g, '')
+
+    // Get the request parameters sent by the client
+    const requestQuery = parsedUrl.query
+
+    // Get the request method
+    const requestMethod = req.method.toLowerCase()
+
+    // Create a new decoder object to decode the POST data from buffer to string
+    const decoder = new StringDecoder('utf-8')
+
+    // Create and empty string to store the decoded buffer
+    let requestData = ''
+
+    // Listen for new POST data and process them
+    req.on('data', function(dataBuffer){
+        // Convert the buffer to string and update the requestData variable
+        const bufferString = decoder.write(dataBuffer)
+        requestData += bufferString
+    })
+
+    // This event will be called when all POST data has been sent
+    // Request should only proceed after post data has been written
+    req.on('end', function(){
+        // Convert any remaining buffer to string and update the requestData variable
+        const bufferString = decoder.end()
+        requestData += bufferString
+
+        // Find the request handler
+        const requestHandler = typeof(routes[requestPath]) === 'function' ? routes[requestPath] : typeof(routes['/' + requestPath]) === 'function' ? routes['/' + requestPath] : null;
+
+        const requestObj = {
+            pathname: '/' + requestPath,
+            method: requestMethod,
+            query: requestQuery,
+            data: JSON.parse(requestData)
+        }
+
+        const responseObj = {}
+
+        responseObj.send = function(content, status){
+            status = typeof(status) === 'number' ? status : 200;
+            res.writeHead(status)
+            res.end(content)
+        }
+
+        // Call the request handler if it exist, else call the notFound handler.
+        if(requestHandler){
+            requestHandler(requestObj, responseObj)
+        }else{
+            routers.notFound(req, res)
+        }
+    })
+})
+
+// Set server to listen to a port
+server.listen(3000, function(){
+    console.log(`Server runing at http://127.0.0.1:3000`);
+})
+
+const routers = {}
+
+routers.welcome = function(req, res){
+    res.send('My Node js API')
+}
+
+routers.users = function(req, res){
+    const allowedMethods = ['GET', 'POST']
+    if(!allowedMethods.includes(req.method.toUpperCase())){
+        return res.send('', 405)
+    }
+    routers._users[req.method](req, res)
+}
+
+routers._users = {}
+
+routers._users.post = function(req, res){
+    const userData = req.data
+    const name = typeof(userData.name) === 'string' && userData.name.trim().length > 0 ? userData.name : false
+    const email = typeof(userData.email) === 'string' ? userData.email.trim() : false
+    const phone = typeof(userData.phone) === 'string' && userData.phone.trim().length > 0 ? userData.phone : false
+    const password = typeof(userData.password) === 'string' && userData.password.length >= 6 ? userData.password : false
+
+    if((name && email && phone && password) === false){
+        return res.send("Please send all required fields", 400)
+    }
+
+    // @Todo: Save the user details to file
+
+    // @Todo Respond with a 201 status
+    res.send(`Your name is ${userData.name}`)
+}
+
+
+routers._users.get = function(req, res){
+    res.send('Get users')
+}
+
+routers.notFound = function(req, res){
+    res.send('Route does not exist')
+}
+
+const routes = {
+    '/': routers.welcome,
+    '/users': routers.users,
+}
